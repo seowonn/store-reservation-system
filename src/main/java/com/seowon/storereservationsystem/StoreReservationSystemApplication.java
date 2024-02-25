5d31b246da2fd24717b501e618fbaf6d7b2c7c8a
손서원 <sonseowon5@gmail.com>
2024-02-25 15:01:48 +0900

    Initial commit

---
+++ src/main/java/com/seowon/storereservationsystem/StoreReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.seowon.storereservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreReservationSystemApplication.class, args);
+	}
+
+}
