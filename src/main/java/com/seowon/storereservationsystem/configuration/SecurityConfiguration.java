ff893d1897f2fd50cbd3fef787ecb82d8a58f1ec
손서원 <sonseowon5@gmail.com>
2024-02-26 00:53:58 +0900

    [001-SignUp] : make login.html page

---
+++ src/main/java/com/seowon/storereservationsystem/configuration/SecurityConfiguration.java
@@ -0,0 +1,13 @@
+package com.seowon.storereservationsystem.configuration;
+
+public class SecurityConfiguration {
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//
+//        http.exceptionHandling()
+//                .accessDeniedPage("/error/denied");
+//
+//        return http.build();
+//    }
+}
