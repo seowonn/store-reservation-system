f883489f8b277b6db2bc04611967d428cbb6e414
손서원 <sonseowon5@gmail.com>
2024-03-10 02:25:45 +0900

    [006 JWT 토큰 추가] feat - JWT 토큰 사용으로 인한 config 및 파일 수정

--- src/main/java/com/seowon/storereservationsystem/config/security/SecurityConfig.java
+++ src/main/java/com/seowon/storereservationsystem/config/security/SecurityConfig.java
@@ -1,32 +1,24 @@
-package com.seowon.storereservationsystem.configuration;
+package com.seowon.storereservationsystem.config.security;
 
-import com.seowon.storereservationsystem.configuration.jwt.JwtAccessDeniedHandler;
-import com.seowon.storereservationsystem.configuration.jwt.JwtAuthenticationEntryPoint;
-import com.seowon.storereservationsystem.configuration.jwt.JwtAuthenticationFilter;
-import com.seowon.storereservationsystem.configuration.jwt.JwtTokenProvider;
-import com.seowon.storereservationsystem.service.UserDetailService;
 import com.seowon.storereservationsystem.type.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Order(1)
+//@Order(1)
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class UserSecurityConfiguration {
+public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -39,30 +31,18 @@ public class UserSecurityConfiguration {
     public SecurityFilterChain userFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .exceptionHandling(exceptionHandling ->
-                        exceptionHandling
-                                .authenticationEntryPoint(
-                                        new JwtAuthenticationEntryPoint())
-                                .accessDeniedHandler(
-                                        new JwtAccessDeniedHandler())
-                )
                 .sessionManagement(configurer ->
                         configurer.sessionCreationPolicy(
                                 SessionCreationPolicy.STATELESS)
                 )
-                .addFilterBefore(
-                        new JwtAuthenticationFilter(jwtTokenProvider),
-                        UsernamePasswordAuthenticationFilter.class
-                )
                 .authorizeHttpRequests(authorizeRequests -> {
                     authorizeRequests
-                            .requestMatchers("/user/register",
-                                    "/user/login", "/login-fail",
-                                    "/logout-success", "/login-success")
+                            .requestMatchers("/api/auth/**",
+                                    "/user/register", "/owner/register")
                             .permitAll();
 
                     authorizeRequests.
-                            requestMatchers("/user/**").authenticated();
+                            requestMatchers("/user/**", "/owner/**").authenticated();
 
                     authorizeRequests
                             .requestMatchers("/review/delete/**")
@@ -70,8 +50,20 @@ public class UserSecurityConfiguration {
                                     Role.USER.getRole(),
                                     Role.OWNER.getRole()
                             );
-
                 })
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint(
+                                        new JwtAuthenticationEntryPoint())
+                                .accessDeniedHandler(
+                                        new JwtAccessDeniedHandler())
+                )
+
+                .addFilterBefore(
+                        new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class
+                )
+
                 .logout(logout -> logout
                         .logoutUrl("/user/logout")
                         .logoutSuccessUrl("/logout-success")
